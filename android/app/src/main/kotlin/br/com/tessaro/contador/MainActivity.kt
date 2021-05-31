3387303d76930be41e1b439a92cbb8297595f073
Cassio <cassioate@gmail.com>
2021-05-31 13:28:51 -0300

    Primeiro app com Flutter

---
+++ android/app/src/main/kotlin/br/com/tessaro/contador/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.tessaro.contador
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(title: "Contador de Pessoas", home: Home()));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int people = 20;
+  String infoText = "Pode Entrar!";
+
+  void changePeople(int delta) {
+    setState(() {
+      people += delta;
+      if (people < 0) {
+        infoText = "Mundo invertido?!";
+      } else if (people <= 10) {
+        infoText = "Poden Entrar!";
+      } else {
+        infoText = "Estamos lotados!";
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Image.asset(
+          "images/restaurant.jpg",
+          fit: BoxFit.cover,
+          height: 1000.0,
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Pessoas: $people",
+              style:
+                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: FlatButton(
+                    child: Text(
+                      "+1",
+                      style: TextStyle(fontSize: 40.0, color: Colors.white),
+                    ),
+                    onPressed: () => changePeople(1),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: FlatButton(
+                    child: Text(
+                      "-1",
+                      style: TextStyle(fontSize: 40.0, color: Colors.white),
+                    ),
+                    onPressed: () => changePeople(-1),
+                  ),
+                ),
+              ],
+            ),
+            Text(
+              infoText,
+              style: TextStyle(
+                  color: Colors.white,
+                  fontStyle: FontStyle.italic,
+                  fontSize: 30.0),
+            )
+          ],
+        )
+      ],
+    );
+  }
+}
